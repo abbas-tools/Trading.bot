542561680ebecea142cd09a6b0c26cdeb7b2ad9f
Abbas Hassan <abbasbaloch626@gmail.com>
2026-08-02 22:42:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1347,4 +1347,5 @@ if __name__ == '__main__':
     bot_thread.start()
     logger.info("✅ Bot started automatically!")
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
+    
